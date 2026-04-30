dad09e8eae56d1f49bee1c2c4441a2a6e7fc877a
BeatrizPortella <84148961+BeatrizPortella@users.noreply.github.com>
2026-04-30 16:59:33 -0300

    Update sketch.ino

--- codigo/sketch.ino
+++ codigo/sketch.ino
@@ -1,26 +1,32 @@
 #define SENSOR_PIN 34
-#define RELE_PIN 26
+#define RELE_PIN 25
 
 int valorUmidade = 0;
 
 void setup() {
   Serial.begin(115200);
+  delay(2000); // garante que o serial inicialize
+
   pinMode(RELE_PIN, OUTPUT);
+
+  Serial.println("Sistema de Irrigacao Iniciado");
 }
 
 void loop() {
   valorUmidade = analogRead(SENSOR_PIN);
 
-  Serial.print("Umidade: ");
+  Serial.print("Umidade do solo: ");
   Serial.println(valorUmidade);
 
+  // Ajuste de limite (pode mexer se quiser)
   if (valorUmidade < 2000) {
     digitalWrite(RELE_PIN, HIGH);
-    Serial.println("Solo seco - Irrigando");
+    Serial.println("Status: SOLO SECO -> Irrigando");
   } else {
     digitalWrite(RELE_PIN, LOW);
-    Serial.println("Solo úmido - Parando irrigação");
+    Serial.println("Status: SOLO UMIDO -> Irrigacao desligada");
   }
 
+  Serial.println("-----------------------------");
   delay(2000);
 }
