5921c22bbd215f60e8e2f256f58f23813295852f
BeatrizPortella <84148961+BeatrizPortella@users.noreply.github.com>
2026-04-30 18:14:17 -0300

    Fix formatting and cleanup in sketch.ino

--- codigo/sketch.ino
+++ codigo/sketch.ino
@@ -6,15 +6,12 @@ int valorUmidade = 0;
 void setup() {
   Serial.begin(115200);
   delay(2000); // garante que o serial inicialize
-
   pinMode(RELE_PIN, OUTPUT);
-
   Serial.println("Sistema de Irrigacao Iniciado");
-}
+} 
 
 void loop() {
   valorUmidade = analogRead(SENSOR_PIN);
-
   Serial.print("Umidade do solo: ");
   Serial.println(valorUmidade);
 
@@ -25,8 +22,8 @@ void loop() {
   } else {
     digitalWrite(RELE_PIN, LOW);
     Serial.println("Status: SOLO UMIDO -> Irrigacao desligada");
-  }
+  } 
 
   Serial.println("-----------------------------");
   delay(2000);
-}
+} 
